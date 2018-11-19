b9aa1810ca53bc19b5ed84547e5b02835ee03e96
ORIENTSOFTWARE\nvdoanh <vandoanh94@gmail.com>
2018-11-19 12:49:33 +0700

    a

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBz8okmqV98NJuwamxpkODDuAiM80eUd44",
+    authDomain: "ionic-firebase-vdoanh.firebaseapp.com",
+    databaseURL: "https://ionic-firebase-vdoanh.firebaseio.com",
+    projectId: "ionic-firebase-vdoanh",
+    storageBucket: "ionic-firebase-vdoanh.appspot.com",
+    messagingSenderId: "330635553725"
+  }
+};
